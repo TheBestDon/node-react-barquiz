c3d03516e93fe2d2a7eb45fc7d09b2f1f52897bc
TheBestDon <donatas_bakanas@yahoo.com>
2017-07-26 01:00:02 +0300

    test

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import assert from 'assert';
 import config from '../config';
 import _ from 'lodash';
 import { mongoose } from '../db/mongoose';
-import {User} from '../model/user';
+import { User } from '../model/user';
 import { authenticate } from '../middleware/authenticate';
 
 let mdb;
